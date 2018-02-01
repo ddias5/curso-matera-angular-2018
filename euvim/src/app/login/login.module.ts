fa26fc1d080ae9c491628379e48370a7c0ef66cf
Vinicius Rufine <vsrufine@gmail.com>
2018-02-01 19:59:59 -0200

    Login

--- euvim/src/app/login/login.module.ts
+++ euvim/src/app/login/login.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
+import { RouterModule } from '@angular/router';
+import { LoginRouting } from './login.routing';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { LoginService } from './login.service';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    LoginRouting,
+    HttpClientModule
+  ],
+  providers: [
+    LoginService,
+    HttpClient
   ],
   declarations: [LoginComponent]
 })
