5a3ded60992c755e1ec9121ff46d11aff5d40dad
Vinicius Rufine <vsrufine@gmail.com>
2018-02-01 22:10:51 -0200

    Popup exibindo erro de requests dos services

--- euvim/src/app/request-error/request-error.interceptor.service.ts
+++ euvim/src/app/request-error/request-error.interceptor.service.ts
@@ -4,25 +4,26 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/do';
 import { RequestErrorComponent } from './request-error.component';
 import { MatDialog } from '@angular/material';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class RequestErrorInterceptor implements HttpInterceptor {
 
-  constructor(private dialog: MatDialog) { }
+  constructor(private dialog: MatDialog, private _router: Router) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-    return next.handle(request)
-      .do((event: HttpEvent<any>) => { }, (err: any) => {
-        if (err instanceof HttpErrorResponse) {
-          this.openDialog({});
-          if (err.status === 401) {
-            this.openDialog({});
-          }
-        } else {
-          // zzz
+    return next.handle(request).do((event: HttpEvent<any>) => { }, (err: any) => {
+      if (err instanceof HttpErrorResponse) {
+        if (err.status === 401) {
+          this.openDialog({ code: 401, message: "Sessão expirada" });
+          this._router.navigate(["/login"]);
         }
-      });
+        this.openDialog({ code: err.status, message: err.message })
+      } else {
+        this.openDialog({ code: "Indisponível", message: "Não foi possível validar a causa do erro" })
+      }
+    });
   }
 
   private openDialog(info) {
@@ -30,4 +31,5 @@ export class RequestErrorInterceptor implements HttpInterceptor {
       data: info
     });
   }
+
 }
