b3215f2e72e3d3c85929287663bfee2c903c186a
ddias5 <danilod11@live.com>
2018-10-08 17:20:04 -0300

    Correção local contrução do form.
    Construtor é o método padrão predefinido da classe typescript. Não há relação entre Angular e constructor, logo é uma boa prática mover a iniciação do form do angular para o onInit.

--- euvim/src/app/main/relatorio/presenca/presenca.component.ts
+++ euvim/src/app/main/relatorio/presenca/presenca.component.ts
@@ -18,15 +18,16 @@ export class PresencaComponent implements OnInit {
     private _formBuilder: FormBuilder,
     private _disciplinaService: DisciplinaService,
     private _relatorioService: RelatorioService
-  ) {
+  ) { }
+
+  ngOnInit() {
+    
     this.form = this._formBuilder.group({
       disciplina: [''],
       dataInicio: [''],
-      dataFim: [''],
-    })
-  }
-
-  ngOnInit() {
+      dataFim: ['']
+    });
+    
     this._disciplinaService
       .listar()
       .subscribe(res => {
